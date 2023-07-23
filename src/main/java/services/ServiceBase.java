f762f2bf84b6550eb1815550bda6da0a6e457771
山本　紗世 <dapokora@outlook.jp>
2023-07-23 13:43:38 +0900

    EmployeeValidator、EmployeeService作成

---
+++ src/main/java/services/ServiceBase.java
@@ -0,0 +1,20 @@
+package services;
+
+import javax.persistence.EntityManager;
+
+import utils.DBUtil;
+
+public class ServiceBase {
+
+    protected EntityManager em = DBUtil.createEntityManager();
+
+    /**
+     * EntityManagerのクローズ
+     */
+    public void close() {
+        if (em.isOpen()) {
+            em.close();
+        }
+    }
+
+}
